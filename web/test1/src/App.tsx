e4caee2f63a95bb9968951e9179633f6c7c93e6f
Vladislav Podyachev <vlad.podyachev001@mail.ru>
2024-05-14 12:34:59 +0300

    modules

--- web/test1/src/App.tsx
+++ web/test1/src/App.tsx
@@ -1,42 +1,13 @@
-import  { useState, useEffect } from 'react';
-import axios from 'axios';
 
-function MyComponent() {
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+import MyComponent from './MyComponent'; 
 
-    useEffect(() => {
-        axios.get('https://localhost:7220/WeatherForecast')
-            .then(response => {
-                setData(response.data);
-            })
-            .catch(error => {
-                setError(error);
-            })
-            .finally(() => {
-                setLoading(false);
-            });
-    }, []);
-
-    if (loading) {
-        return <p>Loading...</p>;
-    }
-
-    if (error) {
-        return <p>Error: {error.message}</p>;
-    }
-
+function App() {
     return (
         <div>
-            <h1>Todo Items</h1>
-            <ul>
-                {data.map(item => (
-                    <li key={item.id}>{item.title}</li>
-                ))}
-            </ul>
+            
+            <MyComponent />
         </div>
     );
 }
 
-export default MyComponent;
+export default App;
